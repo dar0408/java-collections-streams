c0c0486aaa11f75ebdff0da280270e7f8c6f7b6f
dar0408 <dsalgotra2021@gmail.com>
2025-03-30 18:39:09 +0530

    [Darpan] add : Implemented EvenChecker class to check if a number is even

---
+++ jUnit/main/basicTesting/NumberUtils.java
@@ -0,0 +1,11 @@
+package basicTesting;
+
+// Utility class for number-related operations
+public class NumberUtils {
+
+    // Checks if a number is even
+    public boolean isEven(int number) {
+        return number % 2 == 0; // If remainder when divided by 2 is 0, it's even
+    }
+}
+

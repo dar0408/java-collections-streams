d722e3c1a3c5f6a49ddd777978903cff73e9ab8b
dar0408 <dsalgotra2021@gmail.com>
2025-03-30 18:41:18 +0530

    [Darpan] add : Implemented TemperatureConverter for Celsius to Fahrenheit conversion

---
+++ jUnit/main/advanceTesting/TemperatureConverter.java
@@ -0,0 +1,15 @@
+package advanceTesting;
+
+// Class to handle temperature conversions
+public class TemperatureConverter {
+
+    // Converts Celsius to Fahrenheit
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+    // Converts Fahrenheit to Celsius
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+}

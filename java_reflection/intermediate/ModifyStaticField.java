77714ad4997965ecc5f19e630f7758f070ad0044
dar0408 <dsalgotra2021@gmail.com>
2025-04-06 21:09:31 +0530

    [Darpan] add: access and modify private static field API_KEY in Configuration class

---
+++ java_reflection/intermediate/ModifyStaticField.java
@@ -0,0 +1,17 @@
+package intermediate;
+
+import java.lang.reflect.Field;
+
+class Configuration {
+    private static String API_KEY = "OLD_KEY";
+}
+
+public class ModifyStaticField {
+    public static void main(String[] args) throws Exception {
+        Field field = Configuration.class.getDeclaredField("API_KEY");
+        field.setAccessible(true);
+
+        field.set(null, "NEW_SECRET_KEY"); // static fields require null instance
+        System.out.println("Updated API_KEY: " + field.get(null));
+    }
+}
